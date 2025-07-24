64a1453314d4a559c942e5f03fb5edddd4109bb7
devgabrielsborges <dev.gabrielsborges@proton.me>
2025-07-24 00:36:09 -0300

    Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   base: "/",
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 });
